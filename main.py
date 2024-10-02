4a4324cceab20d1a0be917bc56f9b3bf4d9e33b9
ivgorge <ivgorge@yandex-team.ru>
2024-10-02 22:52:07 +0300

    fix

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import asyncio
-from bot import init_bot
+from bot import init_bot, bot
 from images_db.db import (
     get_images_and_last_id, 
     add_images_to_db
 )
+from parse_topfapgirls import scrape_all_pages
 async def main():
-    await init_bot()
+    # await init_bot()
+    id = 'alice-delish-3'
+    name = 'AliceDelish'
+    await scrape_all_pages(id, name, bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
--- parse_topfapgirls.py
+++ parse_topfapgirls.py
@@ -7,30 +7,33 @@ def convert_urls_to_dict(url_list, name):
 
 async def scrape_photos(context, page, bot):
     photo_urls = []
-    photo_items = await page.query_selector_all("div.photo-item a")
+    photo_items = await page.query_selector_all(".model-photo div.photo-item a")
 
     for item in photo_items:
         href = await item.get_attribute('href')
         if href:
             new_page = await context.new_page()
-
-            await new_page.goto('https://www.topfapgirlspics.com/'+href)
-            await new_page.wait_for_load_state('networkidle')
-            
-            img_element = await new_page.query_selector("div.img a img")
-            if img_element:
-                src = await img_element.get_attribute('src')
-                if src:
-                    photo_urls.append(src)
-
-            await new_page.close()
+            try:
+                await new_page.wait_for_timeout(500)
+                await new_page.goto('https://www.topfapgirlspics.com' + href)
+                await new_page.wait_for_selector("div.img a img")
+                    
+                img_element = await new_page.query_selector("div.img a img")
+                if img_element:
+                    src = await img_element.get_attribute('src')
+                    if src:
+                        photo_urls.append(src)
+            except Exception as e:
+                print(f"Error occurred: {e}")
+            finally:
+                await new_page.close()
         
     return photo_urls
 
 async def scrape_all_pages(id, name, bot):
     async with async_playwright() as p:
         browser = await p.chromium.launch(
-            headless=True,
+            headless=False,
             args=[
                 '--disable-gpu',
                 '--no-sandbox',
