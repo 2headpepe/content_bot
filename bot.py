304e2222085e082698b5944f237ebc1d9f61ab9d
ivgorge <ivgorge@yandex-team.ru>
2024-10-12 02:52:28 +0300

    upd

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from aiogram.filters import Command, CommandObject
 from aiogram.types import InputMediaPhoto, KeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, InlineKeyboardButton
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from datetime import datetime
-
+from neuro_img import generate
 from sd import generate_image
 from consts import (
     stable_diffusion_image_path, 
@@ -447,20 +447,21 @@ async def schedule_send_image(extra=False):
         await bot.send_message(feedback_chat_id, f"Не могу выкладывать фото в {channel}, пока вы не посмотрите предложку. Может быть выполним {command}?")
 
 async def init_bot():
-    images_db.db_approved.init_db()
-    init_db()
+    await generate()
+    # images_db.db_approved.init_db()
+    # init_db()
 
-    for i in range(0,23,4):
-        scheduler.add_job(schedule_parse_pinterest_images, "cron", hour=i, minute=55, args=[True]) 
-        scheduler.add_job(schedule_parse_pinterest_images, "cron", hour=i, minute=50, args=[False]) 
+    # for i in range(0,23,4):
+    #     scheduler.add_job(schedule_parse_pinterest_images, "cron", hour=i, minute=55, args=[True]) 
+    #     scheduler.add_job(schedule_parse_pinterest_images, "cron", hour=i, minute=50, args=[False]) 
 
-    scheduler.add_job(schedule_send_image, "cron", hour=10, minute=0, args=[True]) 
-    scheduler.add_job(schedule_send_image, "cron", hour=20, minute=0, args=[False]) 
+    # scheduler.add_job(schedule_send_image, "cron", hour=10, minute=0, args=[True]) 
+    # scheduler.add_job(schedule_send_image, "cron", hour=20, minute=0, args=[False]) 
     
-    scheduler.add_job(schedule_send_image, "cron", hour=10, minute=30, args=[False]) 
-    scheduler.add_job(schedule_send_image, "cron", hour=20, minute=30, args=[True]) 
+    # scheduler.add_job(schedule_send_image, "cron", hour=10, minute=30, args=[False]) 
+    # scheduler.add_job(schedule_send_image, "cron", hour=20, minute=30, args=[True]) 
 
-    scheduler.add_job(post_hot_images,"cron", hour=20, minute=30, args=[tg_hot_channel])
+    # scheduler.add_job(post_hot_images,"cron", hour=20, minute=30, args=[tg_hot_channel])
 
-    scheduler.start()
-    await dp.start_polling(bot)
+    # scheduler.start()
+    # await dp.start_polling(bot)
